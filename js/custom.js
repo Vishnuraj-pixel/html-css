722c5caf8f048c984526adc182dfd38821e3bde3
unknown <Vishnuraj-pixel>
2021-05-30 12:39:02 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+function welcome(){
+    alert("---------------------- Welcome to Neo Technologies -------------------- Sed ut perspiciatis omnis istc natus error sit voluptatcm accusantium doloremque laudantium, totam rem aperiam.");
+    alert("We will keep in touch with you.")
+}
