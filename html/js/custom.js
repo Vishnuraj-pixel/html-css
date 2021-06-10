2257388140f3b445655a393a7ed205b5b0462aa1
unknown <Vishnuraj-pixel>
2021-06-10 18:54:14 +0530

    first-commit

---
+++ html/js/custom.js
@@ -0,0 +1,10 @@
+var slider = document.getElementById('slider');
+var slider = document.getElementById('nn2');
+var slider = document.getElementById('pp2');
+
+nn2.onclick = function(){
+    slider.style.transform = 'translateX(-50%)';
+}
+pp2.onclick = function(){
+    slider.style.transform = 'translateX(50%)';
+}
